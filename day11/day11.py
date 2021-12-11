a48554b7cb7a5ff8b5909e4c68e5ef2f4c858654
Gaggu097 <80213589+Gaggu097@users.noreply.github.com>
2021-12-11 18:47:14 +0100

    power blackout

--- day11/day11.py
+++ day11/day11.py
@@ -9,13 +9,96 @@ def get_input(filename):
     return data
 
 
+
 def make_structure_from_data(data):
+    matrix = []
+    for line in data:
+        lst = []
+        
+        for i in range(0, len(line)):
+            if line[i] != '\n':
+                lst.append(int(line[i]))
+        matrix.append(lst)
     
-    pass
+    return matrix
+
+
+
+def step(matrix):
+    for i in range(0, len(matrix)):
+        for j in range(9, len(matrix[i])):
+           matrix[i][j] += 1
+    return matrix
+
+
+
+def flash(matrix, x, y):
+    if matrix[x][y] > 9:
+        
+        length = len(matrix)
+        height = len(matrix[x])
+
+
+        # center line x
+        matrix[x][y] = 0
+        
+        if y - 1 >= 0:
+                matrix[x][y-1] += 1
+        if y + 1 < height:
+                matrix[x][y+1] += 1
+
+
+
+        # line x - 1
+        if x - 1 >= 0:
+            matrix[x-1][y] += 1
+            if y - 1 >= 0:
+                matrix[x-1][y-1] += 1
+            if y + 1 < height:
+                matrix[x-1][y+1] += 1
+
+
+
+        #down line x + 1
+
+        if x + 1 < length:
+            matrix[x+1][y] += 1
+            if y + 1 < height:
+                matrix[x+1][y+1] += 1
+            if y - 1 <= 0:
+                matrix[x+1][y-1] += 1
+
+
+    return matrix
+
+
+
+
+def check_flashes(matrix):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            if matrix[i][j] > 9:
+                matrix = flash(matrix, i, j)
+
+    return matrix
+
+
+
+def sample11():
+    sample = get_input(sample_file11)
+    matrix = make_structure_from_data(sample)
+    for i in range(0, 3):
+        matrix = step(matrix)
+        matrix = check_flashes(matrix)
+        for element in matrix:
+            print(element)
+        print()
+    #print(matrix)
+
 
 
 def main():
-    pass
+    sample11()
 
 
 if __name__ == "__main__":
