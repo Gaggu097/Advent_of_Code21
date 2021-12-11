6c2096e8b97534804d3b8392f1b0d9445e4e07d5
Gaggu097 <80213589+Gaggu097@users.noreply.github.com>
2021-12-11 17:29:15 +0100

    day11
    doing it

---
+++ day11/day11.py
@@ -0,0 +1,22 @@
+
+sample_file11 = 'day11/sample11'
+
+
+def get_input(filename):
+    with open(filename) as file:
+        data = file.readlines()
+
+    return data
+
+
+def make_structure_from_data(data):
+    
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
